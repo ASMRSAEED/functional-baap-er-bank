390966318979ccff23c67162084b9618492b974a
ASMRSAEED <asmrsaeed@gmail.com>
2023-02-22 01:06:13 +0600

    add-conditions

--- js/deposit.js
+++ js/deposit.js
@@ -1,12 +1,16 @@
 document.getElementById('btn-deposit').addEventListener('click', function () {
     const newDepositAmount = getInputFieldValueById('deposit-field');
-    const previousDepositTotal = getTextElementValueByID('deposit-total');
-    const newDepositTotal = previousDepositTotal + newDepositAmount;
-    setTextElementValueById('deposit-total', newDepositTotal);
-    
-    const previousBalanceTotal = getTextElementValueByID('balance-total');
-    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-    setTextElementValueById('balance-total', newBalanceTotal)
+    if (typeof newDepositAmount !== "number" || isNaN(newDepositAmount) || newDepositAmount <= 0) {
+        alert('Please Deposit Valid Amount')
+    } else {
+        const previousDepositTotal = getTextElementValueByID('deposit-total');
+        const newDepositTotal = previousDepositTotal + newDepositAmount;
+        setTextElementValueById('deposit-total', newDepositTotal);
+
+        const previousBalanceTotal = getTextElementValueByID('balance-total');
+        const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+        setTextElementValueById('balance-total', newBalanceTotal)
+    }
 });
 
 
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,11 +1,20 @@
 document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithdrawAmount = getInputFieldValueById('withdraw-field');
-    const previousWithdrawTotal = getTextElementValueByID('withdraw-total');
-    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-    setTextElementValueById('withdraw-total', newWithdrawTotal);
+    if (typeof newWithdrawAmount !== "number" || isNaN(newWithdrawAmount) || newWithdrawAmount <= 0) {
+        alert('Please Withdraw Valid Amount')
+    } else {
+        const previousWithdrawTotal = getTextElementValueByID('withdraw-total');
+        const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+        const previousBalanceTotal = getTextElementValueByID('balance-total');
+        
+        if (newWithdrawAmount > previousBalanceTotal) {
+            alert('Not Enough Balance')
+        } else {
+            const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+            setTextElementValueById('balance-total', newBalanceTotal);
+            setTextElementValueById('withdraw-total', newWithdrawTotal);
+        }
+    }
 
-    const previousBalanceTotal = getTextElementValueByID('balance-total');
-    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-    setTextElementValueById('balance-total', newBalanceTotal);
 
 })
