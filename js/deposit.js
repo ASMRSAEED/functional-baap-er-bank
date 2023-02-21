3835105a7acb784740dac1767542e6930c4c44b7
ASMRSAEED <asmrsaeed@gmail.com>
2023-02-21 23:35:08 +0600

    deposit-function-created

---
+++ js/deposit.js
@@ -0,0 +1,22 @@
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getElementValueByID(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
+}
+
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+})
+
+
+
+
